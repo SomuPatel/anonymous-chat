cd848dd80541187aa1d7eed6d08229577120b26a
Raj Patel <khemrajpatel4567@gmail.com>
2026-05-25 23:08:38 +0530

    Final Mongo Fix

--- server/server.js
+++ server/server.js
@@ -40,18 +40,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 });
 
-.then(() => {
-
-    console.log("MongoDB Connected");
-
-})
-
-.catch((err) => {
-
-    console.log(err);
-
-});
-
 /* =========================
    USER MODEL
 ========================= */
